594a44c9bd0a858b1295aceae139d488ee0e21c7
Netherwulf <kanclerz3@tlen.pl>
2018-03-03 03:37:09 +0100

    Added roulette selection

--- QAP.py
+++ QAP.py
@@ -1,4 +1,6 @@
 import math
+import random
+
 import numpy as np
 
 
@@ -13,12 +15,19 @@ class QAP(object):
         self.counter = 0
         self.best_fit = math.inf
         self.n = None if file != None else 5
-        self.distance_matrix = None if file != None else np.array([[0, 4, 5, 1, 4], [1, 0, 5, 6, 7], [4, 6, 0, 1, 5], [3, 6, 7, 0, 2], [4, 6, 2, 5, 0]])
-        self.cost_matrix = None if file != None else np.array([[0, 4, 5, 1, 4], [1, 0, 5, 6, 7], [4, 6, 0, 1, 5], [3, 6, 7, 0, 2], [4, 6, 2, 5, 0]])
+        self.distance_matrix = None if file != None else np.array(
+            [[0, 4, 5, 1, 4], [1, 0, 5, 6, 7], [4, 6, 0, 1, 5], [3, 6, 7, 0, 2], [4, 6, 2, 5, 0]])
+        self.cost_matrix = None if file != None else np.array(
+            [[0, 4, 5, 1, 4], [1, 0, 5, 6, 7], [4, 6, 0, 1, 5], [3, 6, 7, 0, 2], [4, 6, 2, 5, 0]])
         self.cur_pop = None
         self.evaluated_pop = None
         self.selected_pop = None
-        # self.old_pop
+        self.min_fitness = None
+        self.max_fitness = math.inf
+        self.evaluation_sum = 0
+        self.new_pop = np.array([])
+        self.sum_of_probabilities = 0.0
+        self.pop_probabilities = np.array([])
         pass
 
     def initialize(self):
@@ -29,26 +38,49 @@ class QAP(object):
         # print(start_pop)
 
     def evaluate(self, chromosome):
-        return sum(sum(self.distance_matrix[i] * self.cost_matrix[chromosome[i]-1] for i in range(5)))
+        return sum(sum(self.distance_matrix[i] * self.cost_matrix[chromosome[i] - 1] for i in range(5)))
 
     def evaluation(self):
         self.evaluated_pop = np.array([self.evaluate(i) for i in self.cur_pop])
+        self.max_fitness = np.amin(self.evaluated_pop) if np.amin(
+            self.evaluated_pop) < self.max_fitness else self.max_fitness
+        self.min_fitness = np.amax(self.evaluated_pop)
+        self.evaluation_sum = sum(self.evaluated_pop)
+        print("Najgorszy: " + str(self.min_fitness) + " Najlepszy: " + str(self.max_fitness))
 
-    def roulette_prob(self):
-        return # UZUPELNIC OBLICZANIE PRAWDOPODOBIENSTWA RULETKA
+    def roulette_prob(self, cur_fitness):
+        return (self.min_fitness - cur_fitness) / self.evaluation_sum
 
-    def selection(self, tour = False):
+    def selection(self, tour=False):
+        self.new_pop = np.empty((0, 5), int)
+        self.pop_probabilities = np.array([])
+        self.sum_of_probabilities = 0.0
+        for j in self.evaluated_pop:
+            probability = self.sum_of_probabilities + self.roulette_prob(j)
+            self.pop_probabilities = np.append(self.pop_probabilities, probability)
+            self.sum_of_probabilities += probability
+        print(self.pop_probabilities)
         if tour:
             # UZUPELNIC SELEKCJĘ TURNIEJOWĄ
             return
         else:
-            return # UZUPELLNIC WYKORZYSTUJAC roulette_prob dla każdego
+            print(self.evaluated_pop)
+            # print(self.pop_probabilities)
+            for k in range(100):
+                r = random.uniform(self.pop_probabilities[0], self.pop_probabilities[99])
+                for i, obj in enumerate(self.pop_probabilities):
+                    if obj < r < self.pop_probabilities[i + 1] and i < 100:
+                        self.new_pop = np.append(self.new_pop, np.array([self.cur_pop[i]]), axis=0)
+                        break
+            # print(self.new_pop)
+            #TEST
+            # self.evaluated_pop = np.array([self.evaluate(i) for i in self.new_pop])
+            print(np.array([self.evaluate(i) for i in self.new_pop]))
+            print("Najlepsi: " + str(sum(self.evaluate(i) == self.max_fitness for i in self.new_pop)) + "\nNajgorsi: " + str(sum(
+                self.evaluate(i) == self.min_fitness for i in self.new_pop)) + "\nLiczba wartoścl: " + str(len(np.unique(self.new_pop))))
 
-    #def ox_crossover(self):
-        #OPERATOR KRZYZOWANIA OX
-    #def crossover(self):
+    # def ox_crossover(self):
+    # OPERATOR KRZYZOWANIA OX
+    # def crossover(self):
 
-    #def mutation(self):
-
-
-
+    # def mutation(self):
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import QAP
 
 qap = QAP.QAP()
 qap.initialize()
-qap.evaluate([3, 5, 2, 1, 4])
+# qap.evaluate([3, 5, 2, 1, 4])
 qap.evaluation()
+qap.selection()
